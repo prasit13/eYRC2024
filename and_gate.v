747d34a80a22434e9ec3237a06c443861b171d83
prasit13 <142914846+prasit13@users.noreply.github.com>
2026-06-11 09:24:11 +0530

    Remove unnecessary comments from and_gate.v

--- and_gate.v
+++ and_gate.v
@@ -11,6 +11,7 @@ endmodule
 //https://drive.google.com/file/d/151-I08VtY72H-55SE-kvH5L9nZ25_6eR/view?usp=sharing
 //https://drive.google.com/file/d/137Pu6LtOGxqcdFvgjhRbKcyGvbY3uY-k/view?usp=sharing
 
+//https://drive.google.com/drive/folders/1cpnlVvMy9w4T56PmRa-Oxoy9zyBuNAu1?usp=sharing
 
 //https://drive.google.com/file/d/1qju2EtYLal-1AlunbxY028Q93G6fAhff/view?usp=sharing
 //https://drive.google.com/file/d/1rgTOX6zoefMTRRjzPoFRVCVSGUG5SMkx/view?usp=sharing
